2eb30d2a2abc9741ad979a670d3b1ec67d3308cc
Kleison Vitt <kleison.vittm@outlook.com>
2026-01-12 10:58:27 -0300

    Prettier Tailwindcss plugin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 const Header = () => {
   return (
-    <header className="w-full pt-4 px-4 md:pt-4 md:px-0 md:max-w-4xl mx-auto">
-      <div className="flex justify-between items-center">
+    <header className="mx-auto w-full px-4 pt-4 md:max-w-4xl md:px-0 md:pt-4">
+      <div className="flex items-center justify-between">
         <img src="casa-hamburguer-logo.png" alt="logo casa hamburguer" />
 
-        <button className="bg-primary py-2 px-9 rounded font-medium cursor-pointer">
+        <button className="bg-primary cursor-pointer rounded px-9 py-2 font-medium">
           Entrar
         </button>
       </div>
