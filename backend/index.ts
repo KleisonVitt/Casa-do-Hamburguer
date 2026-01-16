3094196ca29aaa595609c670c7023c68567083a5
Kleison <kleison.vittm@outlook.com>
2026-01-16 20:16:25 -0300

    configuração básica do back end

---
+++ backend/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.json("Hello World!");
+});
