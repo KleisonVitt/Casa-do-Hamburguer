52d6cfccbb3fb38ef77ba74139b3ff636e38a840
Kleison <kleison.vittm@outlook.com>
2026-01-16 17:40:12 -0300

    Layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,34 @@
 import Login from "./pages/Login.tsx";
 import Register from "./pages/Register.tsx";
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Outlet } from "react-router";
 import HomePage from "./pages/HomePage.tsx";
+import Header from "./components/Header.tsx";
+import Pedidos from "./components/Pedidos.tsx";
+
+const OutletLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+};
 
 export const Router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
+    element: <OutletLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/pedidos",
+        element: <Pedidos />,
+      },
+    ],
   },
+
   {
     path: "/login",
     element: <Login />,
