74e7c77e11db076db65abcf1cfed0cae72ee6ffd
Kleison Vitt <kleison.vittm@outlook.com>
2026-01-20 16:17:18 -0300

    endpoint de login pt1

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,25 @@
 import express from "express";
+import { connection } from "./src/db.js";
+import { prisma } from "./src/db.js";
 
 const app = express();
+app.use(express.json());
+connection();
 const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
 
-app.get("/", (req, res) => {
-  res.json("Hello World!");
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      email: email,
+      password: password,
+    },
+  });
+
+  res.json(user);
 });
