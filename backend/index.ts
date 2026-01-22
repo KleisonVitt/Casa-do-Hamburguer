a699cf3f136e2619c78ab3dccf44a001de679a03
Kleison Vitt <kleison.vittm@outlook.com>
2026-01-22 17:35:03 -0300

    jwt

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,18 @@
-import express, { type Request, type Response } from "express";
+import express from "express";
 import { connection } from "./src/db.js";
 import cors from "cors";
 import { router } from "./src/routes.js";
 
+const origin = "http://localhost:5173";
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: origin,
+    credentials: true,
+  }),
+);
 app.use(router);
 connection();
 const PORT = 3000;
